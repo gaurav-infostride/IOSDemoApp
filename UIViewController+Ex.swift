7fbb0da03ef88edef85506e36441b9e68ff6d885
Gaurav Kumar <gauravkumarraz@gmail.com>
2023-01-21 00:46:18 +0530

    Some Changes

--- UIViewController+Ex.swift
+++ UIViewController+Ex.swift
@@ -11,32 +11,22 @@ import UIKit
 extension  UIViewController {
     func showAlert(title:String = "IOSDemoApp", message:String?){
         let allertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
-//        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { (action) in
-//
-//        }
-//        allertController.addAction(cancelAction)
-        
         let OKAction = UIAlertAction(title: "Ok", style: .default) { (action) in
-            
         }
         allertController.addAction(OKAction)
-        
         present(allertController, animated: true)
     }
     
     
     func showActionAlert(title:String = "IOS Demo App", message:String?){
         let allertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let OKAction = UIAlertAction(title: "Yes", style: .destructive) { (action) in
+        }
         let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { (action) in
 
         }
         allertController.addAction(cancelAction)
-        
-        let OKAction = UIAlertAction(title: "Yes", style: .destructive) { (action) in
-            
-        }
         allertController.addAction(OKAction)
-        
         present(allertController, animated: true)
     }
     
